aec867227ecdf685e64da59a289b66087806abe0
Snow Dev <carlosavila@appeal.com.mx>
2020-02-22 11:41:22 -0600

    GE001
    0. Initial commit.

---
+++ generators/app/index.js
@@ -0,0 +1,57 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    constructor(args, options) {
+        super(args, options);
+    }
+    
+    
+    // First stage
+    async prompting() {
+        this.log("Generator starting... 🤖");
+
+        this.answers = await this.prompt([
+            {
+              type: 'list',
+              name: 'type',
+              message: 'What shall we create today? ',
+              choices: ['screen', 'redux-module'],
+            },
+            {
+              type: 'input',
+              name: 'name',
+              message: 'Input the name for this module => ',
+              validate: input => Boolean(input.length),
+            },
+          ]);
+    }
+    
+    // Seconsd stage
+    writing() {
+        this.log("Writing files... 📝");
+
+        const { type, name } = this.answers;
+        if (type === 'screen') {
+            this.fs.copyTpl(
+                this.templatePath('component.js'),
+                this.destinationPath(`components/${name}.js`),
+                {
+                    name,
+                },
+            );
+        } else {
+            this.fs.copyTpl(
+                this.templatePath('module.js'),
+                this.destinationPath(`modules/${name.toLowerCase()}.js`),
+                {
+                    name,
+                }
+            );
+        }
+    }
+
+    // LAst stage
+    end() {
+        this.log("Bye... 👋");
+    }
+};
